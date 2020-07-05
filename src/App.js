de1dfc486d6e21ff5d259fc49a6d61bbbd253a07
MV <videnin10@gmail.com>
2020-07-05 20:57:06 +0200

    changed App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>Hi There!</p>
+                <span>Deployed after Pull Request!</span>
             </header>
         </div>
     );
